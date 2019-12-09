c82acfec28904393580582be2c36749ff8b2751f
Connor Baxter <20connor.baxter@wildcats.cantonlocal.org>
2019-12-09 09:33:12 -0500

    Added form

--- Schedule.php
+++ Schedule.php
@@ -6,13 +6,18 @@
     $dbname = "workshe3_test";
 	
     $conn = new mysqli($servername,$username,$password,$dbname);
-	$Name = $_GET['id'];
+	$ID = $_GET['id'];
+
+	$sql = "SELECT * From Tutors WHERE ID = '$ID'";
+	$res = $conn->query($sql);
+    $Day = $_GET['day']-1;
 ?>
 <html>
 <head>
 <meta charset="utf-8">
-<title> <?php echo "$Name's"?> Schedule</title>
-<link rel="stylesheet" href="TutorCSS.css?<?php date('l jS \of F Y h:i:s A');?>">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title> <?php echo $row['FirstName'] . $row['LastName']?> Schedule</title>
+<link rel="stylesheet" type="text/css" href="TutorCSS.css?<?php echo date('l jS \of F Y h:i:s A'); ?>" />
 </head>
 <body onLoad="GetCurrentDate();">
 
@@ -56,65 +61,110 @@
 	
 <div class="Wrapper">
  <div class="TimeSlot">
-	
-	<p>Time Schedule Goes Here</p>
+    
+    <div class="TimeBar">
+        <?php
+        
+        //Echos out the times of day and color codes for avalibility
+			
+        	$row = $res->fetch_assoc();
+			$TimesString = $row['Times'];
+            $DaysArray = explode('|', $TimesString);
+			$TimesArray = explode(';', $DaysArray[$Day]);
 		
+			foreach($TimesArray as $Time){
+				
+				$Times = explode(':', $Time);
+				
+				
+				if($Times[1] == 'A'){
+					echo "<div class='TimeHolder tooltip'><div>$Times[0]</div><div class='ColorSlot' style='background-color: greenyellow;' onclick='ChangePage($Times[0]);'></div><span class='tooltiptext tooltip-bottom'>Available</span></div>";
+                    }
+				else if ($Times[1] == 'NA'){
+					echo "<div class='TimeHolder tooltip'><div>$Times[0]</div><div class='ColorSlot' style='background-color: darkred;' onclick=''> </div><span class='tooltiptext tooltip-bottom'>Not Available</span></div>";
+				}
+				else if ($Times[1] == 'TK'){
+					echo "<div class='TimeHolder tooltip'><div>$Times[0]</div><div class='ColorSlot' style='background-color: goldenrod;' onclick=''> </div><span class='tooltiptext tooltip-bottom'>Already Booked</span></div>";
+				}
+				else{
+					echo "<div class='TimeHolder'><div>Error Fetching Schedule</div><div class='ColorSlot' style='background-color: orange;'></div></div>";
+				}
+			}
+        ?>
+    </div>
  </div>
+	
+	<div class="KeyDiv"><div style="background-color: greenyellow;" class='Key tooltip'>Available</div><div style="background-color: goldenrod;" class='Key tooltip'>Already Booked</div><div style="background-color: darkred;" class='Key tooltip'>Not Available</div></div>
+    
+    
+    <div class="InfoForm Flex">
+        <form>
+        1
+            <input type='text' placeholder="Notes" required>
+        </form>
+            <h3 id = 'Name'></h3>
+            <h3 id = 'Subject'></h3>
+            <h3 id = 'Time'></h3>
+    </div>
+	
 </div>
 
 <script>
 
+    var weekdays = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+
 	function OnClick(id){
 		
 		for(var i = 1; i < 8; i++){
-			document.getElementById(i).style = "background-color: background-color: rgba(227,227,227,1.00);;";
+			document.getElementById(i).style = "background-color:rgba(227,227,227,1.00);";
 		}
 		
 		document.getElementById(id).style = "background-color: rgba(204,204,204,1.00);";
 		
-		var Day = null;
-		switch(id){
-			case 1:
-				Day = "Sunday";
-				break;
-				
-			case 2:
-				Day = "Monday";
-				break;
-				
-			case 3:
-				Day = "Tuesday";
-				break;
-				
-			case 4:
-				Day = "Wednesday";
-				break;
-				
-			case 5:
-				Day = "Thursday";
-				break;
-				
-			case 6:
-				Day = "Friday";
-				break;
-				
-			case 7: 
-				Day = "Saturday";
-				break;
-		}
+       
+		var Day = weekdays[id-1];
+		
 
 		document.getElementById("Day").innerHTML = Day;
+        window.location = "Schedule.php?id=<?php echo $ID;?>&day=" + id;
 	}
 	
 	function GetCurrentDate(){
-		var weekdays = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
-		var date = new Date();
-		var CurrentDay = weekdays[date.getDay()];
+        if('<?php echo $_GET['day'];?>' == ''){
+        
+		    var date = new Date();
+		    var CurrentDay = weekdays[date.getDay()];
 		
-		document.getElementById(date.getDay()+1).style = "background-color: rgba(204,204,204,1.00);";
-		document.getElementById("Day").innerHTML = CurrentDay;
+		    document.getElementById(date.getDay()+1).style = "background-color: rgba(204,204,204,1.00);";
+		    document.getElementById("Day").innerHTML = CurrentDay;
+            window.location = "Schedule.php?id=<?php echo $ID;?>&day=" + (date.getDay()+1);
+            ChangeFormInfo();
+        }
+        else {
+        
+            document.getElementById(<?php echo $_GET['day'];?>).style = "background-color: rgba(204,204,204,1.00);";
+		    document.getElementById("Day").innerHTML = weekdays[<?php echo $_GET['day'];?>-1];
+            ChangeFormInfo();
+        }
 	}
+    
+    function ChangePage(TimeID){
+        var id = '<?php echo $ID;?>';
+        
+        ChangeFormInfo(TimeID);
+    }
 	
+    
+    function ChangeFormInfo(Time = 0){
+        var Name = '<?php echo $row['FirstName'] . $row['LastName'];?>';
+        var Subject = '<?php echo $row['Subject']?>';
+        
+        document.getElementById("Name").innerHTML = Name;
+        document.getElementById("Subject").innerHTML = Subject;
+        document.getElementById("Time").innerHTML = Time;
+        
+    }
+    
 </script>
 </body>
 </html>
--- Tutors.php
+++ Tutors.php
@@ -10,9 +10,9 @@
     $sql = "SELECT * FROM Tutors ORDER BY Subject ASC";
     $res = $conn->query($sql);
     
-  /*  
-    $sql = "INSERT INTO Tutors (FirstName, LastName, Subject, Days, Times, Who) VALUES ('Connor', 'Baxter', 'Math', 'Monday', '11-5', 'Madison')";
-     $conn->query($sql);*/
+    
+   /*$sql = "INSERT INTO Tutors (FirstName, LastName, Subject, Days, Times, Who) VALUES ('Connor', 'Baxter', 'Programming', 'Monday', '1:A;2:A;3:NA;4:A;5:TK;6:A;7:A;8:A;9:A;10:NA;11:NA;12:TK;13:TK;14:TK;15:NA;16:A;17:A;18:A;19:A;20:A;21:A;22:A;23:A;24:NA|1:NA;2:NA;3:NA;4:A;5:TK;6:A;7:A;8:TK;9:A;10:NA;11:NA;12:TK;13:TK;14:TK;15:NA;16:TK;17:NA;18:NA;19:A;20:A;21:TK;22:TK;23:A;24:NA|1:A;2:A;3:NA;4:A;5:TK;6:A;7:A;8:A;9:A;10:NA;11:NA;12:TK;13:TK;14:TK;15:NA;16:A;17:A;18:A;19:A;20:A;21:A;22:A;23:A;24:NA|1:NA;2:NA;3:NA;4:A;5:TK;6:A;7:A;8:TK;9:A;10:NA;11:NA;12:TK;13:TK;14:TK;15:NA;16:TK;17:NA;18:NA;19:A;20:A;21:TK;22:TK;23:A;24:NA|1:A;2:A;3:NA;4:A;5:TK;6:A;7:A;8:A;9:A;10:NA;11:NA;12:TK;13:TK;14:TK;15:NA;16:A;17:A;18:A;19:A;20:A;21:A;22:A;23:A;24:NA|1:NA;2:NA;3:NA;4:A;5:TK;6:A;7:A;8:TK;9:A;10:NA;11:NA;12:TK;13:TK;14:TK;15:NA;16:TK;17:NA;18:NA;19:A;20:A;21:TK;22:TK;23:A;24:NA|1:NA;2:NA;3:NA;4:A;5:TK;6:A;7:A;8:TK;9:A;10:NA;11:NA;12:TK;13:TK;14:TK;15:NA;16:TK;17:NA;18:NA;19:A;20:A;21:TK;22:TK;23:A;24:NA', 'Madison')";
+   $conn->query($sql);*/
 
 ?>
 
@@ -20,8 +20,9 @@
 <html>
 <head>
 <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Available Tutors</title>
-<link rel="stylesheet" href="TutorCSS.css?<?php date('l jS \of F Y h:i:s A');?>">
+<link rel="stylesheet" href="TutorCSS.css?<?php date('l jS \of F Y h:i:s A');?>" />
 </head>
 
 <body>
@@ -40,17 +41,18 @@
         
         $OldSubject = $Subject;
         $Subject = $TutorList['Subject'];
+		$ID = $TutorList['ID'];
         
         
         if($Subject == $OldSubject){
-            echo "<tr><td>$FName $LName</td> <td><a href='Schedule.php?id=$FName $LName' class='Link'>View Schedule</a></td></tr>";
+            echo "<tr><td>$FName $LName</td> <td><a href='Schedule.php?id=$ID' class='Link'>View Schedule</a></td></tr>";
         }
         else {
             echo "</table>";
             echo "<h3 class='Text'><b>$Subject</b></h3><br>";
             
             echo "<table>";
-            echo "<tr><td>$FName $LName</td> <td><a href='Schedule.php?id=$FName $LName' class='Link'>View Schedule</a></td></tr>";
+            echo "<tr><td>$FName $LName</td> <td><a href='Schedule.php?id=$ID' class='Link'>View Schedule</a></td></tr>";
         }
      }
      echo "</table>";
